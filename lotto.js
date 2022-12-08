fa20ad13851aa0a53f934eaa406ae97ab2834ace
seungchan <superfly9@encar.com>
2022-12-08 15:27:06 +0900

    feat: 뽑은 카드수 제거 및 인풋 창 추가 - 요구에 따라 카드 몇개 뽑았는지 제거 - 인풋창에 몇 개뽑을지 추가하기로

--- lotto.js
+++ lotto.js
@@ -1,10 +1,14 @@
-let 이번턴에뽑을숫자갯수 = 10;
-let nthGame = 1;
-
-const TOTAL_COUNT = 22;
 const lotto = {
+  howManyPick : 10,
+  nthGame : 1,
+  ORIGINAL_BALL : 22,
+  REMAIN_BALL: 22,
+  SELECTED_BALL : 0,
+  isReset : false,
   btn: document.getElementById('js-create-number'),
   multiBtn: document.getElementById('js-multi-number'),
+  input : document.querySelector('#how_many'),
+  saveBtn : document.querySelector('#save'),
   createNumberRange() {
     const MIN = 1, MAX = 320;
     let nums = [];
@@ -15,10 +19,85 @@ const lotto = {
 
     return nums;
   },
+  makeEl () {
+    const numberList = document.querySelector('.number_list');
+    const printTarget = [...numberList.children];
+    printTarget.forEach(item => item && item.remove());
+
+    console.log('[이번 턴 갯수]:',this.howManyPick)
+    console.log('[이번 턴 몇 번째]:',this.nthGame)
+    for (let i = 0;i< this.howManyPick;i++) {
+      const li  = document.createElement('li')
+      console.log('li',li,'[index]:',i)
+      numberList.appendChild(li)
+    }
+  },
+  reset () {
+    alert('모든 공을 뽑았습니다.초기화가 됩니다.')
+    this.nthGame = 1;
+    this.SELECTED_BALL = 0;
+    this.REMAIN_BALL = this.ORIGINAL_BALL;
+    this.howManyPick = 10;
+    this.isRest = true;
+    
+    const target = document.querySelector('.number_list');
+
+    [...target.children].forEach(item=>item && item.remove());
+
+    new Array(this.howManyPick).fill('?').forEach(item=>{
+      const li = document.createElement('li');
+      li.innerHTML = item;
+      target.appendChild(li)
+    })
+  },
+  printNumber() {
+    if (this.nthGame === 3) {
+      this.howManyPick = 2;
+    }
+    if (this.nthGame > 3) return this.reset();
+
+    this.SELECTED_BALL += this.howManyPick;
+    this.REMAIN_BALL -= this.howManyPick;
+    const nums = this.createNumber();
+
+    this.makeEl();
+    this.isReset = false;
+    this.nthGame+=1;
+
+    const target = document.querySelector('.number_list');
+    // const target2 = document.querySelector('.js-only-paper .game_item');
+    const printTarget = [...target.children];
+    // const paperTarget = [...target2.children];
+    console.log('[printTarget]:',printTarget)
+    printTarget.forEach((item, index) => {item.innerHTML = nums[index]});
+
+    target.classList.add('play');
+    // document.querySelector('.js-only-paper').classList.add('play');
+    
+    const id = setTimeout(() => {
+      target.classList.remove('play');
+      // document.querySelector('.js-only-paper').classList.remove('play');
+      clearTimeout(id)
+    },3000);
+    
+    // for (let i = 0; i < paperTarget.length; i++) {
+    //   paperTarget[i].innerHTML = nums[i];
+    //   printTarget[i].innerHTML = nums[i];
+    // }
+  },
+  printCount () {
+    const selectedSum =  document.querySelector('.count_sum');
+    const current = document.querySelector('.count_current');
+    const remain = document.querySelector('.count_remain');
+
+    selectedSum.innerHTML = this.SELECTED_BALL +'개'
+    current.innerHTML = (this.isReset ? 0  : this.howManyPick ) +'개'
+    remain.innerHTML =  this.REMAIN_BALL + '개'
+  },
   createNumber() {
     const numberRange = this.createNumberRange();
     let nums = [];
-
+    
     function setRandomIndex(num, max) {
       let number = Math.floor(Math.random() * max) + 1; // 1 ~ max까지의 숫자
       return num === number ? setRandomIndex() : number; // 중복발생시 재귀실행
@@ -26,22 +105,19 @@ const lotto = {
 
     numberRange.forEach((_, index, arr) => {
       let randomNum = setRandomIndex();
-      if (index + 1 > 이번턴에뽑을숫자갯수) {
+      if (index + 1 > this.howManyPick) {
         return false;
       } 
-      console.log('[i]:',index) // i: 9이후로 출력 안됨, 고차함수의 콜백도 return이 마찬가지로 적용됨
       if (!arr[randomNum]) {
-        console.log(randomNum)
         let prevNum = randomNum;
         randomNum = setRandomIndex(prevNum, arr.length - 1);
         nums.push(...arr.splice(randomNum, 1));
       } else {
-        console.log('있다')
         nums.push(...arr.splice(randomNum, 1)); // splice =>return 제거한 요소를 담은 배열
       }
     });
     nums.sort((a, b) => a - b );
-    console.log(nums, numberRange)
+    console.log(nums)
     return nums;
   },
   getFormatDate (date) {
@@ -52,7 +128,7 @@ const lotto = {
     let day2 = week[date.getDay()];
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+    let seconds = date.getSeconds() < 10? `0${date.getSeconds()}`: date.getSeconds();
   
     return `${[year, month, day].join('/')} (${day2}) ${[hours, minutes, seconds].join(':')}`;
   },
@@ -68,29 +144,6 @@ const lotto = {
       target.classList.add('on');
     }
     target.innerHTML = `발행일 : ${date}`;
-  },
-  printNumber() {
-    this.makeEl();
-    const nums = this.createNumber();
-    const target = document.querySelector('.number_list');
-    const target2 = document.querySelector('.js-only-paper .game_item');
-    const printTarget = [...target.children];
-    const paperTarget = [...target2.children];
-    
-    target.classList.add('play');
-    document.querySelector('.js-only-paper').classList.add('play');
-    
-    const id = setTimeout(() => {
-      target.classList.remove('play');
-      document.querySelector('.js-only-paper').classList.remove('play');
-      console.log('[id]:',id)
-      clearTimeout(id)
-    },3000);
-    
-    printTarget.forEach((item, index) => {item.innerHTML = nums[index]});
-    for (let i = 0; i < paperTarget.length; i++) {
-      paperTarget[i].innerHTML = nums[i];
-    }
   },
   printMultiNumber() {
     const target = document.querySelector('.js-multi-paper .paper_body');
@@ -113,43 +166,20 @@ const lotto = {
       }
     });
   },
-  makeEl () {
-    nthGame+=1;
-    const numberList = document.querySelector('.number_list');
-    console.log('[chid]:',numberList.children)
-    if (numberList.children.length > 0) {
-      for (i = 0;i< 이번턴에뽑을숫자갯수;i++) {
-        numberList.children[i].remove();
-      }
-    }
-      if (nthGame === 3) {
-        이번턴에뽑을숫자갯수 = 2;
-      }
-      if (nthGame >3) {
-        nthGame = 1;
-        이번턴에뽑을숫자갯수 = 10;
-      }
-    for (let i = 0;i< 이번턴에뽑을숫자갯수;i++) {
-      const li  = document.createElement('li')
-      li.innerHTML = '?';
-      // numberList.firstChild.remove()
-      numberList.appendChild(li)
-    }
-    console.log('[numberList]:',numberList.children)
-  },
   init() {
     const btn = this.btn;
     const multiBtn = this.multiBtn;
-    this.makeEl();
+
 
     btn.addEventListener('click', () => {
       this.printNumber();
-      this.printDate('.publish_date');
+      // this.printCount()
+      // this.printDate('.publish_date');
     });
-    multiBtn.addEventListener('click', () => {
-      this.printMultiNumber();
-      this.printDate('.publish_date2');
-    });
+    // multiBtn.addEventListener('click', () => {
+    //   this.printMultiNumber();
+    //   this.printDate('.publish_date2');
+    // });
   }
 };
 function slideChange() {
@@ -184,4 +214,4 @@ const lottoSlide = () => {
 };
 
 lotto.init();
-lottoSlide();
+// lottoSlide();
